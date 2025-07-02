d4f305106a70838df784e248ff404d3ef855a4f3
Phoebepm <phoebeesmepm@gmail.com>
2025-07-02 15:41:57 +0100

    setting up columns - attempt

--- BD_dash.py
+++ BD_dash.py
@@ -1,21 +1,29 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+
+
 #removed max rows that can be displayed 
 pd.set_option("display.max_rows", None)
 
+#this is what will be displayed on the main page - statistics
 def stats(name_f):
-    gender(name_f)
+    with col1:
+        
+        gender(name_f)
 
 def gender(file):
     data = pd.read_excel("%s"%file,"member_data")
-    #table of numner of m and f
+    #table of number of m and f
     gender_counts = data['sex_life_1'].value_counts()
     num_women = gender_counts.get('F', 0)
     num_men = gender_counts.get('M',0)
+    num_people = gender_counts.get(not 'M' or not 'F',1)
 
     #calc percentage of m and f
-    total = num_men + num_women
+    #add validation to - check no null values
+    total = num_men + num_women + num_people
     percent_f = round(num_women / total,2)
     percent_m = round(num_men / total,2)
     #table - python dictioary
@@ -34,6 +42,9 @@ st.set_page_config(
     layout = "wide",
     initial_sidebar_state= 'auto')
 
+col1, col2, col3 = st.columns(0.4,0.3,0.3,gap ='small',vertical_alignment= 'top')
+
+
 with st.sidebar:
     st.header('Files')
     st.header('File upload')
@@ -43,7 +54,10 @@ with st.sidebar:
     
     if uploaded_file is not None:
         f_name = uploaded_file.name
+        #when pressed will display statistics on the file - file name on button
         st.button(label = uploaded_file.name, on_click = lambda f_name: stats(f_name))
+
+
         
 
 
