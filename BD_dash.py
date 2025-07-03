99ae609bdbc198d0e3da8a761d1f072b8c36e5f5
Phoebepm <phoebeesmepm@gmail.com>
2025-07-03 12:11:50 +0100

    removed table and added pie chart

--- BD_dash.py
+++ BD_dash.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
 import numpy as np
 
 
@@ -15,6 +15,7 @@ def stats(name_f):
 
 def gender(file):
     data = pd.read_excel("%s"%file,"member_data")
+    #st.write('**Sex Distribution**')
     #table of number of m and f
     gender_counts = data['sex_life_1'].value_counts()
     num_women = gender_counts.get('F', 0)
@@ -27,26 +28,14 @@ def gender(file):
     label_pie_g = 'Male','Female','Other'
     colour_pie = 'b','r','g'
     #makes code thread safe
-    fig, ax = plt.subplots()
-    ax.pie(sizes,labels = label_pie_g, colours = colour_pie)
-    st.pyplot(fig)
+    fig = px.pie(
+        names = label_pie_g,
+        values = sizes,
+        title = 'Sex Distribution',
 
-    #calc percentage of m and f
-    #add validation to - check no null values
-    total = num_men + num_women + num_other
-    percent_f = round(num_women / total,2)
-    percent_m = round(num_men / total,2)
-    percent_o = round(num_other/ total,2)
-
-    #table - python dictioary
-    gender_f = {
-        'Gender': ['Male','Female','Other'],
-        'Count': [num_men, num_women, num_other],
-        'Percent': [percent_m, percent_f, percent_o]
-    }
-    #display  data as a table
-    df = pd.DataFrame(gender_f)
-    st.dataframe(df)
+    )
+    fig.update_traces(textinfo = 'none', hovertemplate='%{label}: %{percent}' )
+    st.plotly_chart(fig)
 
 
 st.set_page_config(
@@ -55,7 +44,7 @@ st.set_page_config(
     initial_sidebar_state= 'auto')
 
 #column set up
-col1, col2, col3 = st.columns([0.4,0.3,0.3], gap = 'small')
+col1, col2, col3 = st.columns([0.3,0.4,0.3], gap = 'small')
 
 #setting up sidebar
 with st.sidebar:
